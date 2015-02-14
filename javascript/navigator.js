254763187b77a2cf72e650d1b71b78709e613d82
Peng Jingwen <pengsrc@icloud.com>
2015-02-14 17:12:38 +0800

    Improve Navigitor
    Signed-off-by: Peng Jingwen <pengsrc@icloud.com>

--- javascript/navigator.js
+++ javascript/navigator.js
@@ -1,3 +1,4 @@
+
 function toggle_travel_mobile() {
 
     var travel_mobile_list = $(".travel-mobile");
@@ -5,7 +6,7 @@ function toggle_travel_mobile() {
     if (travel_mobile_list.style.display == "block") {
         travel_mobile_list.style.display = "none"
     } else {
-        travel_mobile_list.style.display = "block"
+        travel_mobile_list.style.display = "black"
     }
 
 }
